8d89fbafedc6fa565c6006b429f8c307b395e397
Harshith Valluru <33281061+HarshithValluru@users.noreply.github.com>
2020-09-10 18:53:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Using GitHub-Webhook. Trial-1 !!");
+		System.out.println("Using GitHub-Webhook. Trial-2 !!");
 	}
 }
