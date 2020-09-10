80faca74a5ab5f875836b5962a59212177c89928
Harshith Valluru <33281061+HarshithValluru@users.noreply.github.com>
2020-09-10 17:52:09 +0530

    Added web-hook in sop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Hello Harshith From Jenkins Training @Edgeverveudemy.com");
+		System.out.println("Hello Harshith From GitHub-Webhook");
 	}
 }
